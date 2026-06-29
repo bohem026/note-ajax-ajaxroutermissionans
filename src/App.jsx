69ae8b30e4bf2851eb794d7f8b130fe0695fc3da
bohem dev <microbin98@gmail.com>
2026-06-29 10:23:35 +0900

    feat: add routing with React Router and implement basic layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router';
+import Layout from './components/Layout';
+import Home from './pages/Home';
 
 function App() {
-
+  const [posts, setPosts] = useState([]);
+  const [loaded, setLoaded] = useState(false);
   return (
     <>
-     
+      <Routes>
+        <Route path="/" element={<Layout loaded={loaded} />}>
+          <Route index element={<Home posts={posts} />} />
+          {/* <Route path="posts" element={<Posts posts={posts} />} />
+        <Route
+          path="posts/:id"
+          element={<PostDetail posts={} onDelete={}/>}
+        />
+        <Route path="*" element={<NotFound />}/> */}
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
